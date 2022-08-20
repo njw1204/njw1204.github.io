90f7f5a8eb60777349f647f7a69875189bf3896b
Na Jongwoo <njw1204@naver.com>
2022-08-20 19:15:26 +0900

    Updates

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 const getArmyPercentage = () => {
-  return (
-    Math.min(
-      100,
-      (+(new Date()) - +(new Date('2020-09-11T00:00:00+09:00')))
-      / (+(new Date('2022-08-10T00:00:00+09:00')) - +(new Date('2020-09-11T00:00:00+09:00'))) * 100
-    )
+  return Math.min(
+    Number.MAX_SAFE_INTEGER,
+    ((+new Date() - +new Date("2020-09-11T00:00:00+09:00")) /
+      (+new Date("2022-08-10T00:00:00+09:00") -
+        +new Date("2020-09-11T00:00:00+09:00"))) *
+      100
   ).toFixed(7);
 };
 
 window.setInterval(() => {
   const percentage = getArmyPercentage();
-  document.querySelector(".army-progress").value = percentage;
-  document.querySelector(".army-progress-caption").innerText = percentage + "%";
+  document.querySelector(".army-progress").value = Math.min(100, percentage);
+  document.querySelector(".army-progress-caption").innerText = `${percentage}%`;
 }, 20);
