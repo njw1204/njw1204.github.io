a2deb4545c1288d19157476a5c22652c1f4fcdfc
Na Jongwoo <njw1204@naver.com>
2022-09-14 16:51:39 +0900

    Update dday

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,18 @@
-const getArmyPercentage = () => {
+const getDdayPercentage = (dDayStartDate, dDayEndDate) => {
   return Math.min(
     Number.MAX_SAFE_INTEGER,
-    ((+new Date() - +new Date("2020-09-11T00:00:00+09:00")) /
-      (+new Date("2022-08-10T00:00:00+09:00") -
-        +new Date("2020-09-11T00:00:00+09:00"))) *
+    ((+new Date() - Number(dDayStartDate)) /
+      (Number(dDayEndDate) - Number(dDayStartDate))) *
       100
-  ).toFixed(7);
+  ).toFixed(8);
 };
 
 window.setInterval(() => {
-  const percentage = getArmyPercentage();
-  document.querySelector(".army-progress").value = Math.min(100, percentage);
-  document.querySelector(".army-progress-caption").innerText = `${percentage}%`;
+  const percentage = getDdayPercentage(
+    new Date("2018-03-02T00:00:00+09:00"),
+    new Date("2025-03-01T00:00:00+09:00")
+  );
+  document.querySelector(".dday").title = "Graduation Counter";
+  document.querySelector(".dday-progress").value = Math.min(100, percentage);
+  document.querySelector(".dday-progress-caption").innerText = `${percentage}%`;
 }, 20);
